7e0ce0d81ba16b2cd8cbef8916e3fe298eb29c21
amanda.kellen <amandakellensoares@hotmail.com>
2020-01-17 11:48:15 -0200

    exercício de arquivos

--- Arquivos.py
+++ Arquivos.py
@@ -19,29 +19,30 @@ if os.path.isfile("ex1.txt") == False:
     print("Arquivo ex1.txt nao existe")
 else:
     meuArquivo = open('ex1.txt', 'r')
-    meuArquivo = meuArquivo.readlines()
+    arq = meuArquivo.readlines()
 
-    for i in range(len(meuArquivo)):
+    for i in range(len(arq)):
 
-        if meuArquivo[i] == "200.135.80.9\n":
+        if arq[i] == "200.135.80.9\n" or arq[i] == "192.168.1.1\n" or arq[i] == "8.35.67.74\n" or arq[i] == "1.2.3.4\n":
             if cont == 0:
                 endereços_validos = open('endereços_validos.txt', 'w')
-                endereços_validos.write(meuArquivo[i])
+                endereços_validos.write(arq[i])
                 endereços_validos.close()
             else:
                 endereços_validos = open('endereços_validos.txt', 'a')
-                endereços_validos.write(meuArquivo[i])
+                endereços_validos.write(arq[i])
                 endereços_validos.close()
             cont += 1
 
-        elif meuArquivo[i] == "257.32.4.5\n":
+        elif arq[i] == "257.32.4.5\n" or arq[i] == "85.345.1.2\n" or arq[i] == "9.8.234.5\n" or arq[
+            i] == "192.168.0.256\n":
             if cont2 == 0:
-                endereços_invalidos = open('endereços_validos.txt', 'w')
-                endereços_invalidos.write(meuArquivo[i])
+                endereços_invalidos = open('endereços_invalidos.txt', 'w')
+                endereços_invalidos.write(arq[i])
                 endereços_invalidos.close()
             else:
-                endereços_invalidos = open('endereços_validos.txt', 'a')
-                endereços_invalidos.write(meuArquivo[i])
+                endereços_invalidos = open('endereços_invalidos.txt', 'a')
+                endereços_invalidos.write(arq[i])
                 endereços_invalidos.close()
             cont2 += 1
 endereços_validos.close()
